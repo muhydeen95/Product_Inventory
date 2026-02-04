d3ba7bc02fe1c1acec6cc9cb589e5e5d45deda1f
ALABI MUHYDEEN OLANIYI <48595796+muhydeen95@users.noreply.github.com>
2026-02-04 16:07:07 +0100

    index html file added

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const router = require('./routes/product.route.js');
 const app = express();
 app.use(express.json())
 
+app.use(express.json());
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send("hello");
 });
